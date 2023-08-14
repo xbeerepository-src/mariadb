cf7469a4dd49407af0385b9b56d15de337d8b3fb
Eric Fischer <efischer78@yahoo.fr>
2023-08-14 23:18:43 +0200

    ok

--- templates/up.sh
+++ templates/up.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+mysql_install_db --basedir=/usr --datadir={{ .datadir }} --user={{ .xbee.user }} && chown -R {{ .xbee.user }}:{{ .xbee.group }} {{ .datadir }}
+install -v -m755 -o {{ .xbee.user }} -g {{ .xbee.group }} -d /run/mysqld
+
 
 mysqld_safe --user={{ .xbee.user }} 2>&1 >/dev/null &
 sleep 1s
@@ -9,11 +12,5 @@ sleep 1s
 spawn mysqladmin -u root password
 expect {New password: } {send "{{ .password }}\n"}
 expect {Confirm new password: } {send "{{ .password }}\n"}
-expect eof
-EOF
-
-/usr/bin/expect <<EOF
-spawn mysqladmin -p shutdown
-expect {Enter password:} {send "{{ .password }}\n"}
 expect eof
 EOF
