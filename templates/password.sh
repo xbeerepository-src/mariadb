014ae5009d9b83685aaa4394108c913fc182b820
Eric Fischer <efischer78@yahoo.fr>
2023-08-14 21:00:56 +0200

    ok

--- templates/password.sh
+++ templates/password.sh
@@ -2,18 +2,18 @@
 set -e
 
 
-mysqld_safe --user={{ .pack.xbeeUser }} 2>&1 >/dev/null &
+mysqld_safe --user={{ .xbee.user }} 2>&1 >/dev/null &
 sleep 1s
 
 /usr/bin/expect <<EOF
 spawn mysqladmin -u root password
-expect {New password:} {send "{{ .pack.password }}\n"}
-expect {Confirm new password:} {send "{{ .pack.password }}\n"}
+expect {New password: } {send "{{ .password }}\n"}
+expect {Confirm new password: } {send "{{ .password }}\n"}
 expect eof
 EOF
 
 /usr/bin/expect <<EOF
 spawn mysqladmin -p shutdown
-expect {Enter password:} {send "{{ .pack.password }}\n"}
+expect {Enter password:} {send "{{ .password }}\n"}
 expect eof
 EOF
